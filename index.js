7d60ec34c375f0b2a417e04dcb4f81aeb883c2bb
Neiver97 <neiver.tapia@udea.edu.co>
2019-03-25 03:09:23 -0500

    Listo para entregar!!!! GREAT JOB

---
+++ index.js
@@ -0,0 +1,2 @@
+/*La verdad no pude implementar Js ya que  investigué lo mínimo y no encontré espacio para adaptarlo al código,
+voy a seguir estudiandolo más a fondo, pero pienso que alcancé a utilizarlo con bootstrap ¡Gracias por compartir los conocimientos!*/
